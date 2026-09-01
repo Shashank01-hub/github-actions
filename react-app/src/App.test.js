89e996bce091f5fce695ea53ce44a80f5179814a
Shashank Chauhan <chauhan.shashank987@gmail.com>
2026-09-02 00:32:45 +0530

    Update test to expect element to be in the document

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -4,7 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const element = screen.queryByText(/learn react/i);
-  expect(element).not.toBeInTheDocument();
-  expect(screen.queryByText(/learn react/i)).not.toBeInTheDocument();
-  
+  expect(element).toBeInTheDocument();  
 });
