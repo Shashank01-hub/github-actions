93998170ae0f5824ae35e7b5cec104a3950d792c
Shashank Chauhan <chauhan.shashank987@gmail.com>
2026-09-02 00:16:09 +0530

    Update App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -5,5 +5,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).not.toBeInTheDocument();
-  expect(screen.queryByText(/learn react/i)).not.toBeInTheDocument();
 });
